064b42a10a11aacb4a4bfe11211918a514b3a2b9
beto-machado <luisrobertobeto3@gmail.com>
2022-02-23 13:33:09 -0300

    added axios, tailwind, upload.svg

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "Arial", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
